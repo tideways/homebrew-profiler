7dab0bd119cc107905b0bcf19e194345326bbb9d
Benjamin Eberlei <kontakt@beberlei.de>
2016-07-08 11:16:18 +0200

    Fix mix-up

--- Formula/tideways-daemon.rb
+++ Formula/tideways-daemon.rb
@@ -3,8 +3,8 @@ require "formula"
 class TidewaysDaemon < Formula
     homepage 'https://tideways.io'
 
-    url 'https://s3-eu-west-1.amazonaws.com/qafoo-profiler/downloads/tideways-daemon_macos_i386-1.3.7.tar.gz'  if MacOS.prefer_64_bit?
-    url 'https://s3-eu-west-1.amazonaws.com/qafoo-profiler/downloads/tideways-daemon_macos_amd64-1.3.7.tar.gz'  if not MacOS.prefer_64_bit?
+    url 'https://s3-eu-west-1.amazonaws.com/qafoo-profiler/downloads/tideways-daemon_macos_amd64-1.3.7.tar.gz'  if MacOS.prefer_64_bit?
+    url 'https://s3-eu-west-1.amazonaws.com/qafoo-profiler/downloads/tideways-daemon_macos_i386-1.3.7.tar.gz'  if not MacOS.prefer_64_bit?
     sha1 '589bb94d2230c67c6394db1fc2b00d1ab594b72d' if MacOS.prefer_64_bit?
     sha1 '0153183f4e120a21b11344c6d5067289538ca655' if not MacOS.prefer_64_bit?
 
