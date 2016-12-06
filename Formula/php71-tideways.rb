e5134023234d82cc1a9e30dd6c2aa335644fbac7
Bastian Hofmann <bastian.hofmann@researchgate.net>
2016-12-06 23:58:16 +0100

    Add PHP 7.1 formula

---
+++ Formula/php71-tideways.rb
@@ -0,0 +1,15 @@
+require File.expand_path("../../Abstract/abstract-tideways-php-extension.rb", __FILE__)
+
+class Php71Tideways < AbstractTidewaysPhpExtension
+    init
+    homepage 'https://github.com/tideways/php-profiler-extension'
+    head 'https://github.com/tideways/php-profiler-extension.git'
+    url 'https://github.com/tideways/php-profiler-extension/archive/v4.0.5.zip'
+    version 'v4.0.5'
+
+    def self.init opts=[]
+        super()
+        depends_on "php71" => opts if build.with?('homebrew-php')
+    end
+end
+
