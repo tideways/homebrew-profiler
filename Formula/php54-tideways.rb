7a207932cdae4ced1025d9dc4d3a7ff351d950c7
Benjamin Eberlei <kontakt@beberlei.de>
2017-11-18 22:43:21 +0100

    Update for tideways v4.1.4 and add PHP 7.2 version.

--- Formula/php54-tideways.rb
+++ Formula/php54-tideways.rb
@@ -4,7 +4,7 @@ class Php54Tideways < AbstractTidewaysPhpExtension
     init
     homepage 'https://github.com/tideways/php-profiler-extension'
     head 'https://github.com/tideways/php-profiler-extension.git'
-    url 'https://github.com/tideways/php-profiler-extension/archive/v4.1.2.zip'
-    sha256 '620d01adf00dec0f46940c5af52adcd12e3aab94845873e3d9296ec44f56d017'
-    version 'v4.1.2'
+    url 'https://github.com/tideways/php-profiler-extension/archive/v4.1.4.zip'
+    sha256 '8897c04084cdb2e4f5520a70f61113d27cf8a6081d5726f50a71b338ca10c3f9'
+    version 'v4.1.4'
 end
