9122c64304342721562f2c3a8ae5ef68159cef78
Benjamin Eberlei <kontakt@beberlei.de>
2018-02-02 19:47:05 +0100

    Update path to php-xhprof-extension.

--- Formula/php70-tideways.rb
+++ Formula/php70-tideways.rb
@@ -2,9 +2,9 @@ require File.expand_path("../../Abstract/abstract-tideways-php-extension.rb", __
 
 class Php70Tideways < AbstractTidewaysPhpExtension
     init
-    homepage 'https://github.com/tideways/php-profiler-extension'
-    head 'https://github.com/tideways/php-profiler-extension.git'
-    url 'https://github.com/tideways/php-profiler-extension/archive/v4.1.4.zip'
+    homepage 'https://github.com/tideways/php-xhprof-extension'
+    head 'https://github.com/tideways/php-xhprof-extension.git'
+    url 'https://github.com/tideways/php-xhprof-extension/archive/v4.1.4.zip'
     sha256 '8897c04084cdb2e4f5520a70f61113d27cf8a6081d5726f50a71b338ca10c3f9'
     version 'v4.1.4'
 end
