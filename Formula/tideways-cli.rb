dec8f6aa33cf14a8597751f2f13685b043f31c80
Benjamin Eberlei <kontakt@beberlei.de>
2018-02-19 17:14:06 +0100

    tideways-cli release v0.3.3

--- Formula/tideways-cli.rb
+++ Formula/tideways-cli.rb
@@ -3,11 +3,11 @@ require "formula"
 class TidewaysCli < Formula
     homepage 'https://tideways.io'
 
-    url 'https://s3-eu-west-1.amazonaws.com/tideways/cli/0.3.1/tideways-cli_0.3.1_macos_amd64.tar.gz' if MacOS.prefer_64_bit?
-    url 'https://s3-eu-west-1.amazonaws.com/tideways/cli/0.3.1/tideways-cli_0.3.1_macos_i386.tar.gz' if not MacOS.prefer_64_bit?
-    sha256 'd7d2379e6e5e360c342fc0e61aa932c08eacfa22205f439f549208563828e788' if MacOS.prefer_64_bit?
-    sha256 'dbc5538458d485938f25285092eead8e1c6cdfeae5aa72ea6d6441ed8f79cdb5' if not MacOS.prefer_64_bit?
-    version 'v0.3.1'
+    url 'https://s3-eu-west-1.amazonaws.com/tideways/cli/0.3.3/tideways-cli_0.3.3_macos_amd64.tar.gz' if MacOS.prefer_64_bit?
+    url 'https://s3-eu-west-1.amazonaws.com/tideways/cli/0.3.3/tideways-cli_0.3.3_macos_i386.tar.gz' if not MacOS.prefer_64_bit?
+    sha256 '021571645f7564fee9810db488b3e1d3f9dfbfa886e566027d0d078d6d6124ba' if MacOS.prefer_64_bit?
+    sha256 '58de57d79cc64ca5c8d05d89d9cefc39d4984225515abdc74bf51df6391d03b9' if not MacOS.prefer_64_bit?
+    version 'v0.3.3'
 
     def install
         bin.install "tideways"
