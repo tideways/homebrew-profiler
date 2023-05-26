4947e2f8b0d9cf264c1cc0851b758511df62bb1f
Benjamin Eberlei <kontakt@beberlei.de>
2023-05-26 17:55:12 +0200

    Add PHP 8.0-8.2 Formulas based on shivammathur/homebrew-extensions as template.

---
+++ Formula/tideways-php@8.2.rb
@@ -0,0 +1,17 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-tideways-php-extension", __dir__)
+
+class TidewaysPhpAT82 < AbstractTidewaysPhpExtension
+  init
+  desc "Tideways PHP Profiler"
+  homepage "https://tideways.com"
+  url "https://s3-eu-west-1.amazonaws.com/tideways/extension/5.5.18/tideways-php-5.5.18-macos-arm.tar.gz"
+  sha256 "70362477568fd38edbc0178978481b6feed04b0b8d34667b77ef014dd3033010"
+
+  def install
+    prefix.install "tideways-php-#{php_version}.so"
+    write_config_file
+  end
+end
