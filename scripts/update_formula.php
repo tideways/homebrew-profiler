d9b8faf2dec2f865f5492590ab806340472030b7
Benjamin Eberlei <kontakt@beberlei.de>
2024-01-11 22:20:02 +0100

    Adjust update_formula.php to current formula.

--- scripts/update_formula.php
+++ scripts/update_formula.php
@@ -1,6 +1,6 @@
 <?php
 
-$phpVersions = ['8.0', '8.1', '8.2'];
+$phpVersions = ['8.0', '8.1', '8.2', '8.3'];
 
 $formulaTemplate = <<<FORMULA
 # typed: false
@@ -10,8 +10,15 @@ require File.expand_path("../Abstract/abstract-tideways-php-extension", __dir__)
 
 class TidewaysPhpAT{PHP} < AbstractTidewaysPhpExtension
   init
-  url "https://s3-eu-west-1.amazonaws.com/tideways/extension/{VERSION}/tideways-php-{VERSION}-macos-arm.tar.gz"
-  sha256 "{HASH}"
+
+  if Hardware::CPU.arm?
+    url "https://s3-eu-west-1.amazonaws.com/tideways/extension/{VERSION}/tideways-php-{VERSION}-macos-arm.zip"
+    sha256 "{HASH_ARM}"
+  else
+    url "https://s3-eu-west-1.amazonaws.com/tideways/extension/{VERSION}/tideways-php-{VERSION}-macos-x86.zip"
+    sha256 "{HASH_X64}"
+  end
+
   version "{VERSION}"
 
   def install
@@ -23,23 +30,24 @@ FORMULA;
 
 $currentVersions = json_decode(file_get_contents("https://app.tideways.io/api/current-versions"), true);
 $extensionVersion = $currentVersions['php']['version'] ?? throw new RuntimeException("Current Tideways extension version not found in current versions payload.");
+$architectures = ['arm', 'x86'];
+$hashes = [];
 
-$archive = file_get_contents('https://s3-eu-west-1.amazonaws.com/tideways/extension/' . $extensionVersion . '/tideways-php-' . $extensionVersion . '-macos-arm.tar.gz');
+foreach ($architectures as $architecture) {
+    $archive = file_get_contents('https://s3-eu-west-1.amazonaws.com/tideways/extension/' . $extensionVersion . '/tideways-php-' . $extensionVersion . '-macos-' . $architecture . '.zip');
 
-if ($archive === false) {
-    throw new RuntimeException("Could not download MacOS archive for Tideways extension version " . $extensionVersion);
+    if ($archive === false) {
+        throw new RuntimeException("Could not download MacOS archive for Tideways extension version " . $extensionVersion);
+    }
+
+    $hashes[$architecture] = hash('sha256', $archive);
 }
 
-$archiveFile = sys_get_temp_dir() . '/macos.tar.gz';
-file_put_contents($archiveFile, $archive);
-
-$tarGz = new PharData($archiveFile);
-$tarGz->decompress()->extractTo(sys_get_temp_dir());
 
 foreach ($phpVersions as $phpVersion) {
     file_put_contents(__DIR__ . '/../Formula/tideways-php@' . $phpVersion, str_replace(
-        ['{PHP}', '{VERSION}', '{HASH}'],
-        [str_replace($phpVersion, '.', ''), $extensionVersion, $hash],
+        ['{PHP}', '{VERSION}', '{HASH_ARM}', '{HASH_X86}'],
+        [str_replace($phpVersion, '.', ''), $extensionVersion, $hashes['arm'], $hashes['x86']],
         $formulaTemplate
     ));
 }
