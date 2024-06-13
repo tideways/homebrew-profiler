b5a5088752e0138cc7f726ca753225599030c72e
beberlei <beberlei@users.noreply.github.com>
2024-06-13 17:05:06 +0000

    Updated PHP Extension Formulas to latest version

--- Formula/tideways-php@8.0.rb
+++ Formula/tideways-php@8.0.rb
@@ -7,14 +7,14 @@ class TidewaysPhpAT80 < AbstractTidewaysPhpExtension
   init
 
   if Hardware::CPU.arm?
-    url "https://tideways.s3.amazonaws.com/extension/5.9.2/tideways-php-5.9.2-macos-arm.tar.gz"
-    sha256 "2cf274c47ead926deb10dd2fa9db425c2ec0508b73a3f715ddb656c28ffa149a"
+    url "https://tideways.s3.amazonaws.com/extension/5.10.0/tideways-php-5.10.0-macos-arm.tar.gz"
+    sha256 "4a15450e820ecfd3ee76e5e71ddb09d2429d850a29f313400ea609281bee5143"
   else
-    url "https://tideways.s3.amazonaws.com/extension/5.9.2/tideways-php-5.9.2-macos-x86.tar.gz"
-    sha256 "f529c8cf5d7a085a9365fee5f4a33a3ee00949d77cb084d277198ebd4349813e"
+    url "https://tideways.s3.amazonaws.com/extension/5.10.0/tideways-php-5.10.0-macos-x86.tar.gz"
+    sha256 "6dab90691d916d9491e9373d1be055532c35fbf3dbbaffc49694450354d5f630"
   end
 
-  version "5.9.2"
+  version "5.10.0"
 
   def install
     prefix.install "tideways-php-#{php_version}.so"
