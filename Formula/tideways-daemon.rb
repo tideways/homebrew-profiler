d2827cd0cc399fbf6aa5bbc5727b61c010c73fb9
Tim Düsterhus <tim@tideways-gmbh.com>
2024-07-04 10:56:40 +0200

    Update to tideways-daemon 1.9.2

--- Formula/tideways-daemon.rb
+++ Formula/tideways-daemon.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class TidewaysDaemon < Formula
     homepage 'https://tideways.com'
-    version '1.8.40'
+    version '1.9.2'
 
     if Hardware::CPU.arm?
-      url 'https://tideways.s3.amazonaws.com/daemon/1.8.40/tideways-daemon_macos_arm64-1.8.40.tar.gz'
-      sha256 '669ddeb9487ec5e946176729589233bf6bcff9677d8ceebaf6817b7f26c81035'
+      url 'https://tideways.s3.amazonaws.com/daemon/1.9.2/tideways-daemon_macos_arm64-1.9.2.tar.gz'
+      sha256 '434f1f1b46c038b810b727d850dfadfe2136d0e00ed1a4200e34d847fbb3c78b'
     else
-      url 'https://tideways.s3.amazonaws.com/daemon/1.8.40/tideways-daemon_macos_amd64-1.8.40.tar.gz'
-      sha256 '99de699ceda9c472efd8ce0bbaa9bf60a01dae73286f34c728a0c026f7361501'
+      url 'https://tideways.s3.amazonaws.com/daemon/1.9.2/tideways-daemon_macos_amd64-1.9.2.tar.gz'
+      sha256 '7b04577b15891881860553e7097be6280db705cf52d7931f1e9869c9ad7cf6ba'
     end
 
     def install
