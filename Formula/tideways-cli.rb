9568ecbbc8cbd5b3bede49953dd3ea40a37bc5c7
Tim Düsterhus <tim@tideways-gmbh.com>
2024-07-04 14:51:11 +0200

    Reuse the `version` field within the download URL

--- Formula/tideways-cli.rb
+++ Formula/tideways-cli.rb
@@ -2,13 +2,13 @@ require "formula"
 
 class TidewaysCli < Formula
     homepage 'https://tideways.com'
-    version 'v1.1.0'
+    version '1.1.0'
 
     if Hardware::CPU.arm?
-        url 'https://tideways.s3.amazonaws.com/cli/1.1.0/tideways-cli_macos_arm64-1.1.0.tar.gz'
+        url "https://tideways.s3.amazonaws.com/cli/#{version}/tideways-cli_macos_arm64-#{version}.tar.gz"
         sha256 'bc4e2346129bde1b5c5ba4a30fbfb6b1ac8ab9e9af9236bf0eecc277139ad312'
     else
-        url 'https://tideways.s3.amazonaws.com/cli/1.1.0/tideways-cli_macos_amd64-1.1.0.tar.gz'
+        url "https://tideways.s3.amazonaws.com/cli/#{version}/tideways-cli_macos_amd64-#{version}.tar.gz"
         sha256 'a09bc9f06717101892b120fe92dedf27702be5189a98fdce979a1348ca8b68ad'
     end
 
--- Formula/tideways-php@8.1.rb
+++ Formula/tideways-php@8.1.rb
@@ -5,16 +5,15 @@ require File.expand_path("../Abstract/abstract-tideways-php-extension", __dir__)
 
 class TidewaysPhpAT81 < AbstractTidewaysPhpExtension
   init
+  version "5.10.2"
 
   if Hardware::CPU.arm?
-    url "https://tideways.s3.amazonaws.com/extension/5.10.2/tideways-php-5.10.2-macos-arm.tar.gz"
+    url "https://tideways.s3.amazonaws.com/extension/#{version}/tideways-php-#{version}-macos-arm.tar.gz"
     sha256 "03f48d53e7b5ffd4ce4fe1a862c1a0fda784394fa4a871e1cc29a722f1811e2a"
   else
-    url "https://tideways.s3.amazonaws.com/extension/5.10.2/tideways-php-5.10.2-macos-x86.tar.gz"
+    url "https://tideways.s3.amazonaws.com/extension/#{version}/tideways-php-#{version}-macos-x86.tar.gz"
     sha256 "957adea7387c6ec906f9e15a3ce0e12b7ace028dd4fa4be3e69a173f7a355c8a"
   end
-
-  version "5.10.2"
 
   def install
     prefix.install "tideways-php-#{php_version}.so"
