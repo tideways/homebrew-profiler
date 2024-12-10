021cf4d329c436882f0a3f2edd75fa654372b738
Tim Düsterhus <tim@tideways-gmbh.com>
2024-12-10 16:01:18 +0100

    Manually add Formula/tideways-php@8.4.rb
    The `git diff` in update.yml does not pick up untracked files and thus did not
    automatically create the `tideways-php@8.4.rb` formula.
    
    see 07b90b63a037887a9e83f2a7f80249317582d1b2

---
+++ Formula/tideways-php@8.4.rb
@@ -0,0 +1,22 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-tideways-php-extension", __dir__)
+
+class TidewaysPhpAT84 < AbstractTidewaysPhpExtension
+    init
+    version "5.16.2"
+    checksum = {
+        "arm" => "22a43482d2c0c7c95505c71a0ecba6f351669185c7290e737f6ec5d98f87e398",
+        "x86" => "15a75f754443bf145200716fa2417cf6a603e44bcd750c0a42c331841aacfe0e",
+    }
+
+    arch = Hardware::CPU.arm? ? "arm" : "x86"
+    url "https://tideways.s3.amazonaws.com/extension/#{version}/tideways-php-#{version}-macos-#{arch}.tar.gz"
+    sha256 checksum[arch]
+
+    def install
+        prefix.install "tideways-php-#{php_version}.so"
+        write_config_file
+    end
+end
