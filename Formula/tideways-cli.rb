5291e58743613beb6370f2bc28735d427cb9af86
Tim Düsterhus <tim@tideways-gmbh.com>
2025-06-04 10:21:27 +0200

    Add support for Linuxbrew

--- Formula/tideways-cli.rb
+++ Formula/tideways-cli.rb
@@ -4,13 +4,22 @@ class TidewaysCli < Formula
     homepage 'https://tideways.com'
     version "1.2.10"
     checksum = {
-        "arm64" => "c6cb5373ccb4dee5a6377dce8bc87bce6a0c9b85ed63c925ded68b95a2426269",
-        "amd64" => "0792bb0af055752a6045f3d7c58ada3c86709305bf9319643e6683132d5c3ae1",
+        "macos-arm64" => "c6cb5373ccb4dee5a6377dce8bc87bce6a0c9b85ed63c925ded68b95a2426269",
+        "macos-amd64" => "0792bb0af055752a6045f3d7c58ada3c86709305bf9319643e6683132d5c3ae1",
+        "linux-arm64" => "6bfd07eb83522591b68b1242782029d520ffda5e871824f5a040934ac81fbb61",
+        "linux-amd64" => "7545b05fed2b0d071efc0925ac153800b4529876c19db4101a552d7caf85a1e8",
     }
 
-    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
-    url "https://tideways.s3.amazonaws.com/cli/#{version}/tideways-cli_macos_#{arch}-#{version}.tar.gz"
-    sha256 checksum[arch]
+    if OS.linux?
+        os = "linux"
+        arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+    else
+        os = "macos"
+        arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+    end
+
+    url "https://tideways.s3.amazonaws.com/cli/#{version}/tideways-cli_#{os}_#{arch}-#{version}.tar.gz"
+    sha256 checksum["#{os}-#{arch}"]
 
     def install
         bin.install "tideways"
--- Formula/tideways-daemon.rb
+++ Formula/tideways-daemon.rb
@@ -4,13 +4,22 @@ class TidewaysDaemon < Formula
     homepage 'https://tideways.com'
     version "1.9.40"
     checksum = {
-        "arm64" => "69bed9014607e049ba2ae13f56554b22d7f7374c8cbc846e2677fbbce1831ac6",
-        "amd64" => "8b97de3104434bc1c65dbff618d02765abd0c2aeb7d7e6f9794df8f4958fb260",
+        "macos-arm64" => "69bed9014607e049ba2ae13f56554b22d7f7374c8cbc846e2677fbbce1831ac6",
+        "macos-amd64" => "8b97de3104434bc1c65dbff618d02765abd0c2aeb7d7e6f9794df8f4958fb260",
+        "linux-aarch64" => "d98ab61ef42804f3c3bc4c993e3c324e38dcfdb6f8fb33abc249ea1d49d9b237",
+        "linux-amd64" => "e94e95abdd3cb49991e09cd995b2bfaa395f0a5fe25ab08838936f521d17bda8",
     }
 
-    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
-    url "https://tideways.s3.amazonaws.com/daemon/#{version}/tideways-daemon_macos_#{arch}-#{version}.tar.gz"
-    sha256 checksum[arch]
+    if OS.linux?
+        os = "linux"
+        arch = Hardware::CPU.arm? ? "aarch64" : "amd64"
+    else
+        os = "macos"
+        arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+    end
+
+    url "https://tideways.s3.amazonaws.com/daemon/#{version}/tideways-daemon_#{os}_#{arch}-#{version}.tar.gz"
+    sha256 checksum["#{os}-#{arch}"]
 
     def install
         bin.install 'tideways-daemon'
